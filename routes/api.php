ba1f6c34fe69af28747fc3194ccfd82050c35173
paiboonmt <paiboon500@gmail.com>
2024-10-31 15:50:16 +0700

    up

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\Api\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::controller(UserController::class)->group(function(){
+    Route::get('/users','index');
+});
