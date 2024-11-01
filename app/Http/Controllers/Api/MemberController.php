79e8c40716e10f1d7c1814ba0044453d4b40af52
paiboonmt <paiboon500@gmail.com>
2024-11-01 09:48:08 +0700

    up

---
+++ app/Http/Controllers/Api/MemberController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class MemberController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $members = DB::table('member')->limit(100)->get();
+        return response()->json($members);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $member = DB::table('member')->where('id',$id)->first();
+        return response()->json($member);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\MemberController;
+use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -10,4 +12,13 @@ Route::get('/user', function (Request $request) {
 
 Route::controller(UserController::class)->group(function(){
     Route::get('/users','index');
+});
+
+Route::controller(ProductController::class)->group(function(){
+    Route::get('/products','index');
+});
+
+Route::controller(MemberController::class)->group(function(){
+    Route::get('/members','index');
+    Route::get('/member/{id}','show');
 });
