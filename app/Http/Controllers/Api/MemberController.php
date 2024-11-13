0768b617ce4d081e854af65b03a58db71820d448
paiboonmt <paiboon500@gmail.com>
2024-11-13 10:18:02 +0700

    update countCustomer

--- app/Http/Controllers/Api/MemberController.php
+++ app/Http/Controllers/Api/MemberController.php
@@ -22,8 +22,7 @@ class MemberController extends Controller
     {
         $date = date('Y-m-d');
         $countCustomer = DB::table('member')
-            ->where('status','=' ,'4')
-            ->where('status','=' ,'2')
+            ->where('status','=' ,'4' || '2')
             ->where('exp_date', '>=' ,$date)
             ->count();
         return response()->json($countCustomer);
